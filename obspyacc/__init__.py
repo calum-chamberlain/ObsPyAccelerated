0c1d5a20d9ac4dbaf2d4487928863b06678c5045
Calum Chamberlain <calum.chamberlain@vuw.ac.nz>
2020-06-09 03:50:32 +0000

    gpulib standardising

--- obspyacc/__init__.py
+++ obspyacc/__init__.py
@@ -3,3 +3,12 @@ ObsCuPy: CUDA accelerated ObsPy
 """
 
 __version__ = "0.0.1a"
+
+try:
+    import cupy as gpulib
+except:
+    import clpy as gpulib
+finally:
+    raise NotImplementedError("Neither cupy nor clpy installed.")
+
+# TODO: Monkey patching, import mapping
